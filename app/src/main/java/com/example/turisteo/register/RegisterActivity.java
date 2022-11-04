b6db639d7bbbb9b96b7eaecbe917987ce9925cbc
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2022-11-04 19:41:00 -0300

    Implementación inicial de pantalla de registro

--- app/src/main/java/com/example/turisteo/register/RegisterActivity.java
+++ app/src/main/java/com/example/turisteo/register/RegisterActivity.java
@@ -2,9 +2,12 @@ package com.example.turisteo.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.turisteo.R;
+import com.example.turisteo.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,5 +15,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView tv_goToLogin = findViewById(R.id.tv_goToLogin);
+
+        // Si ya tiene una cuenta
+        tv_goToLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            //finish();
+        });
     }
 }
