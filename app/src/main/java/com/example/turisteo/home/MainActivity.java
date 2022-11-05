a9321bee3b9288baeb7bfa9ff0bd37ff9a006eb9
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2022-11-05 14:12:33 -0300

    Creación de fragments y uso del bottom navigation

--- app/src/main/java/com/example/turisteo/home/MainActivity.java
+++ app/src/main/java/com/example/turisteo/home/MainActivity.java
@@ -1,16 +1,63 @@
 package com.example.turisteo.home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.turisteo.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    HomeFragment homeFragment = new HomeFragment();
+    PlaceFragment placeFragment = new PlaceFragment();
+    FavoritesFragment favoritesFragment = new FavoritesFragment();
+    MapFragment mapFragment = new MapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = findViewById(R.id.bottom_navigation);
+        // El siguiente metodo funciona pero esta deprecado, ver si encuentro otro:
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Al iniciar cargo el fragment de inicio
+        loadFragment(homeFragment);
+    }
+
+    // Metodo para detectar que boton se presiona en el bottom navigation y cargar el fragment correspondiente
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            // Cargo el fragment que corresponde segun el boton que se presiona en el bottom navigation
+            switch (item.getItemId()){
+                case R.id.homeFragment:
+                    loadFragment(homeFragment);
+                    return true;
+                case R.id.placeFragment:
+                    loadFragment(placeFragment);
+                    return true;
+                case R.id.favoritesFragment:
+                    loadFragment(favoritesFragment);
+                    return true;
+                case R.id.mapFragment:
+                    loadFragment(mapFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    // Metodo que reemplaza el fragment correspondiente en el FrameLayout (de id "frame_container") definido en el xml
+    public void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
     }
 }
